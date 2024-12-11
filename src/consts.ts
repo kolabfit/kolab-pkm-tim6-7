ccfc74c8d1dcef5662b3cd29a64a4e27f70f194e
rakhagans37 <muhammadrakha3704@gmail.com>
2024-12-11 13:57:41 +0700

    Merapikan .env dan const agar lebih clean

--- src/consts.ts
+++ src/consts.ts
@@ -6,6 +6,12 @@ export const BRAND_NAME = "Komunitas Halal Bandung";
 export const SITE_TITLE = "Komunitas Halal Bandung Repository";
 export const SITE_DESCRIPTION = "An Astro Theme for Visual Storytelling";
 export const LOGO_URL = "/logo.png";
+export const BRAND_EMAIL = "komunitashalalbandung@gmail.com";
+export const LOCATION_URL_EMBED = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d990.0438737440439!2d107.652457!3d-6.988599!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e9e9e0fb20c7%3A0xc2b331f47416ac3d!2sGriya%20Permata%20Asri!5e0!3m2!1sid!2sid!4v1733818644367!5m2!1sid!2sid%22%20width=%22600%22%20height=%22450%22%20style=%22border:0;%22%20allowfullscreen=%22%22%20loading=%22lazy%22%20referrerpolicy=%22no-referrer-when-downgrade%22";
+export const LOCATION_URL = "https://maps.app.goo.gl/jDAnncoEmYFKeGf2A"
+export const BRAND_PHONE = "+62 856-2394-568";
+export const BRAND_WHATSAPP_URL = "https://api.whatsapp.com/send?phone=628562394568&text=halo%20saya%20ingin%20bertanya%20tentang%20komunitas%20halal%20bandung";
+export const BRAND_INSTAGRAM_URL = "https://www.instagram.com/komunitashalal.bdg?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
 
 // Tags Page Metadata, src/pages/tags/index.astro
 export const Tags_TITLE = "AstroVerse - All Tags";
@@ -35,26 +41,34 @@ export const HeaderLinks = [
   { href: "/contact/", title: "Kontak" },
 ];
 
-// Footer Links, src/components/Footer.astro
-export const FooterLinks = [
-  { href: "/posts/why-astro/", title: "Astro" },
-  { href: "/posts/tailwind-typography/", title: "Tailwind" },
-  { href: "/tags/", title: "Tags" },
-];
+// // Footer Links, src/components/Footer.astro
+// export const FooterLinks = [
+//   { href: "mailto:" + BRAND_EMAIL, title: "Email" },
+//   {}
+// ];
 
 // Social Links, src/components/Footer.astro
 export const SocialLinks = [
-  { href: "/rss.xml", icon: "tabler:rss", label: "RSS" },
   {
-    href: "https://twitter.com/inotelab",
-    icon: "tabler:brand-twitter",
-    label: "Twitter",
+    href: BRAND_INSTAGRAM_URL,
+    icon: "tabler:brand-instagram",
+    label: "Instagram",
   },
   {
-    href: "https://github.com/inotelab",
-    icon: "tabler:brand-github",
-    label: "GitHub",
+    href: BRAND_WHATSAPP_URL,
+    icon: "tabler:brand-whatsapp",
+    label: "Whatsapp",
   },
+  {
+    href: LOCATION_URL,
+    icon: "tabler:map-pin",
+    label: "Location",
+  },
+  {
+    href: "mailto:" + BRAND_EMAIL,
+    icon: "tabler:mail",
+    label: "Email",
+  }
 ];
 
 // Search Page Metadata, src/pages/search.astro
