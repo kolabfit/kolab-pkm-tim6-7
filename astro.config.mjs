b26b112c635c1e3d37689e332cdf640ae299229b
rakhagans37 <muhammadrakha3704@gmail.com>
2024-12-17 23:21:04 +0700

    Memperbaiki astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   site: "https://verse-astro.vercel.app/",
   trailingSlash: "always",
   prefetch: {
